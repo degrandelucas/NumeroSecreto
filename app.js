e87bb1a54d508fa55a4443e6e0c8d56790d74629
degrandelucas <lucasdegrande15@gmail.com>
2025-01-10 18:38:59 -0300

    upgrad with business rule (if)

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 alert('Bem vindo ao Jogo do Numero Secreto!');
+let numeroSecreto = 29;
+let numeroEscolhido = prompt('Escolha o numero entre 1 e 30: ');
+
+if (numeroSecreto == numeroEscolhido) {
+    alert('Parabéns, você acertou!');
+} else {
+    alert('Que pena, tente novamente!');
+}
