d6354a0c86d9f4d89c7d9f66292b46d680ea1f47
degrandelucas <lucasdegrande15@gmail.com>
2025-01-11 07:26:59 -0300

    inclusao do numero de tetantivas para acerto pelo usuario

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ alert('Bem vindo ao Jogo do Numero Secreto!');
 let nome = prompt('Qual é o seu nome?');
 let numeroSecreto = 29;
 let numeroEscolhido = 0;
+let tentativas = 0;
 
 // Controle de Fluxo (Estruturas de Controle)
 do {
     numeroEscolhido = prompt('Escolha o numero entre 1 e 30: ');
+    tentativas++;
     if (numeroSecreto == numeroEscolhido) {
-        alert('Parabéns ' + nome + ' você acertou o numero secreto ' + numeroSecreto);
+        alert('Parabéns ' + nome + ' você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + ' tentativas!');
     } else
         if (numeroEscolhido > numeroSecreto) {
             alert('O numero secreto é menor que o escolhido ' + numeroEscolhido);
