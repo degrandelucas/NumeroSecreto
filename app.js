8f83cacd428432367793118d4c0e7d2618bc4ca8
degrandelucas <lucasdegrande15@gmail.com>
2025-01-11 07:16:22 -0300

    correcao bug para entrar no bloco de dica do numeroEscolhido em relacao do numeroSecreto

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ let numeroEscolhido = 0;
 do {
     numeroEscolhido = prompt('Escolha o numero entre 1 e 30: ');
     if (numeroSecreto == numeroEscolhido) {
-        alert('Parabéns ' + nome + ' você acertou o numero secreto!' + numeroSecreto);
-    } else {
-        alert('Que pena, tente novamente!');
+        alert('Parabéns ' + nome + ' você acertou o numero secreto ' + numeroSecreto);
+    } else
         if (numeroEscolhido > numeroSecreto) {
-            alert('O numero secreto é menor!');
+            alert('O numero secreto é menor que o escolhido ' + numeroEscolhido);
         } else {
-            alert('O numero secreto é maior!');
+            alert('O numero secreto é maior que o escolhido ' + numeroEscolhido);
         } 
-}   
 } while (numeroEscolhido != numeroSecreto);
