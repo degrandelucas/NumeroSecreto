325f5d779b8b146c0f72da6a07140e907636d8a4
degrandelucas <lucasdegrande15@gmail.com>
2025-01-13 10:34:05 -0300

    alterando o numero secreto para numero aleatorio

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 alert('Bem vindo ao Jogo do Numero Secreto!');
 let nome = prompt('Qual é o seu nome?');
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random() * 30 + 1);
 let numeroEscolhido = 0;
 let tentativas = 0;
+let palavraTentativa;
 
 // Controle de Fluxo (Estruturas de Controle)
 do {
     numeroEscolhido = prompt('Escolha o numero entre 1 e 30: ');
     tentativas++;
+    palavraTentativa = tentativas > 1 ? ' tentativas' : ' tentativa';
     if (numeroSecreto == numeroEscolhido) {
-        alert('Parabéns ' + nome + ' você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + ' tentativas!');
-    } else
-        if (numeroEscolhido > numeroSecreto) {
+        alert('Parabéns ' + nome + ' você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + palavraTentativa);
+    }
+    else if (numeroEscolhido > numeroSecreto) {
             alert('O numero secreto é menor que o escolhido ' + numeroEscolhido);
         } else {
             alert('O numero secreto é maior que o escolhido ' + numeroEscolhido);
         } 
-} while (numeroEscolhido != numeroSecreto);
+}
+while (numeroEscolhido != numeroSecreto);
