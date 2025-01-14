9a9ec8c4a36a5585f6ea9755fe60e49cc398138c
degrandelucas <lucasdegrande15@gmail.com>
2025-01-14 09:16:21 -0300

    adicionando variavel numeroMaximo para limite do jogo, sendo mudado apenas na variavel e não em outros lugares do código

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 alert('Bem vindo ao Jogo do Numero Secreto!');
 let nome = prompt('Qual é o seu nome?');
-let numeroSecreto = parseInt(Math.random() * 30 + 1); // base 10 (decimal)
+let numeroMaximo = 50;
+let numeroSecreto = parseInt(Math.random() * 50 + 1); // base 10 (decimal)
 let numeroEscolhido = 0;
 let tentativas = 0;
 let palavraTentativa;
 
 // Controle de Fluxo (Estruturas de Controle)
 do {
-    numeroEscolhido = prompt('Escolha o numero entre 1 e 30: ');
+    numeroEscolhido = prompt(`Escolha o numero entre 1 e ${numeroMaximo}: `);
     tentativas++;
     palavraTentativa = tentativas > 1 ? ' tentativas' : ' tentativa';
     if (numeroSecreto == numeroEscolhido) {
-        alert('Parabéns ' + nome + ' você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + palavraTentativa);
+        alert('Parabéns ' + nome + ', você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + palavraTentativa);
     }
     else if (numeroEscolhido > numeroSecreto) {
             alert('O numero secreto é menor que o escolhido ' + numeroEscolhido);
