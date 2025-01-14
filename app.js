0a339d03ba578226e7f17beb17bb7ac08ee31856
degrandelucas <lucasdegrande15@gmail.com>
2025-01-14 17:00:48 -0300

    adicionando estilo

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-alert('Bem vindo ao Jogo do Numero Secreto!');
-let nome = prompt('Qual é o seu nome?');
 let numeroMaximo = 50;
 let numeroSecreto = parseInt(Math.random() * 50 + 1); // base 10 (decimal)
 let numeroEscolhido = 0;
@@ -8,16 +6,16 @@ let palavraTentativa;
 
 // Controle de Fluxo (Estruturas de Controle)
 do {
-    numeroEscolhido = prompt(`Escolha o numero entre 1 e ${numeroMaximo}: `);
+    numeroEscolhido = console.log(`Escolha o numero entre 1 e ${numeroMaximo}: `);
     tentativas++;
     palavraTentativa = tentativas > 1 ? ' tentativas' : ' tentativa';
     if (numeroSecreto == numeroEscolhido) {
-        alert('Parabéns ' + nome + ', você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + palavraTentativa);
+        console.log('Parabéns, você acertou o numero secreto ' + numeroSecreto + ' em ' + tentativas + palavraTentativa);
     }
     else if (numeroEscolhido > numeroSecreto) {
-            alert('O numero secreto é menor que o escolhido ' + numeroEscolhido);
+        console.log('O numero secreto é menor que o escolhido ' + numeroEscolhido);
         } else {
-            alert('O numero secreto é maior que o escolhido ' + numeroEscolhido);
+            console.log('O numero secreto é maior que o escolhido ' + numeroEscolhido);
         } 
 }
 while (numeroEscolhido != numeroSecreto);
